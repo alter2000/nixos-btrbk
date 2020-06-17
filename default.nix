ee2394422a4694ecbeeed8d7eac5113f59f42ae4
Jor Bashllari <altnate2000@gmail.com>
2020-06-17 18:31:06 +0200

    add switches for all but in* and outvolumes

--- default.nix
+++ default.nix
@@ -1,50 +1,68 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 let
-  btrcfg = ./btrbk.conf;
-  basebcfg = baseNameOf btrcfg;
-  btrbk = pkgs.btrbk;
+  cfg = config.services.btrbk;
   internalMount = "/mnt/raw";
   externalMount = "/mnt/backup";
 in
-
+with lib;
 {
-  environment.etc = {
-    # TODO: parametrize
-    basebcfg.source = btrcfg;
-  };
+  options.services.btrbk = {
+    enable = mkEnableOption "enable btrbk - a btrfs backup service";
 
-  fileSystems = {
-    # TODO: parametrize
-    # TODO: need to build attrset, not list
-
-    "${internalMount}" = {
-      device = "/dev/disk/by-label/lunix";
-      neededForBoot = false;
-      options = [ "subvolid=5" "nofail" "x-systemd.device-timeout=15" ];
-    };
-
-    "${externalMount}" = {
-      device = "/dev/disk/by-label/bups";
-      neededForBoot = false;
-      options = [ "subvolid=5" "nofail" "x-systemd.device-timeout=15" ];
-    };
-  };
-
-  systemd.services = {
-
-    # TODO: parametrize
-    btrbk-snapshot = {
-      description = "btrbk snapshot + transfer service";
-      preStart = "${btrbk}/bin/btrbk clean -c ${basebcfg}";
-      postStart = "${pkgs.btrfs-progs}/bin/btrfs filesystem sync ${externalMount}";
-      serviceConfig = {
-        ExecStart = "${btrbk}/bin/btrbk -v -c ${basebcfg} run";
-        Type = "oneshot";
+    fileSystems = mkOption {
+      # type = types.what?;
+      description = "List of filesystems that need to be mounted for btrbk to run properly.";
+      example = {
+        "/mnt/from" = {
+          neededForBoot = true;
+          device = "/dev/disk/by-label/fsroot";
+          options = [ "subvolid=5" "nofail" ];
+        };
+        "/mnt/external" = {
+          neededForBoot = false;
+          device = "/dev/disk/by-label/backup-hdd";
+          options = [ "subvolid=5" "nofail" "x-systemd.device-timeout=15" ];
+        };
       };
-      startAt = "daily";
-      unitConfig.RequiresMountsFor = [ "${externalMount}" ];
     };
 
+    package = mkOption {
+      type = types.package;
+      default = pkgs.btrbk;
+      example = pkgs.btrbk;
+      description = "The package providing /bin/btrbk.";
+    };
+
+    configFile = mkOption {
+      type = types.path;
+      default = ./btrbk.conf;
+      description = "The btrbk.conf file to use for this instance.";
+    };
+  };
+
+  config = {
+    inherit (cfg) fileSystems;
+
+    environment.etc = {
+      # TODO: parametrize
+      "${baseNameOf cfg.configFile}".source = cfg.configFile;
+    };
+
+    systemd.services = {
+
+      # TODO: parametrize
+      btrbk-snapshot = {
+        description = "btrbk snapshot + transfer service";
+        preStart = "${cfg.package}/bin/btrbk clean -c ${baseNameOf cfg.configFile}";
+        # postStart = "${pkgs.btrfs-progs}/bin/btrfs filesystem sync ${externalMount}";
+        serviceConfig = {
+          ExecStart = "${cfg.package}/bin/btrbk -v -c ${cfg.configFile} run";
+          Type = "oneshot";
+        };
+        startAt = "daily";
+        unitConfig.RequiresMountsFor = [ "${externalMount}" ];
+      };
+    };
   };
 }
